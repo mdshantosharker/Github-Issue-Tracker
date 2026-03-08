a268072365b1d8dcaacefdaedb10c3645574a8f2
mdshantosharker <mdshantosharker@gmail.com>
2026-03-08 09:59:03 +0600

    added duynamic modal

--- script/home.js
+++ script/home.js
@@ -29,7 +29,8 @@ const allIssuesDisplay = (issues, id) => {
     if (id === "all" || issue.status == id) {
       const card = document.createElement("div");
       card.innerHTML = `
-        <div class="card bg-base-100 flex flex-col h-full shadow-sm  border-t-4 ${issue.status === "open" ? " border-green-400" : " border-purple-400"} ">
+      
+        <div onclick="modalBtn(${issue.id})" class="card bg-base-100 flex flex-col h-full shadow-sm  border-t-4 ${issue.status === "open" ? " border-green-400" : " border-purple-400"} ">
               <div class="p-4">
                 <div class="flex justify-between items-center pb-2">
                   <img  src= "${issue.status === "open" ? "/assets/Open-Status.png" : "/assets/Closed- Status .png"}" alt="" />
@@ -42,26 +43,26 @@ const allIssuesDisplay = (issues, id) => {
 
 
                 </div>
-                <h1 class="font-semibold text-xl  pb-2 title">
+                <h1 class="font-semibold text-xl  pb-2 title line-clamp-1">
                   ${issue.title}
                 </h1>
-                <p class="text-[#64748B]  pb-5 description text-sm">
+                <p class="text-[#64748B]  pb-5 description text-sm line-clamp-1">
                   ${issue.description}
                 </p>
     
-                <div class="flex  items-center gap-5 pb-2">
+                <div class="flex flex-wrap   items-center gap-2 pb-2 pt-5">
                   <div
                     class="bg-red-200 rounded-2xl px-2 py-1  flex items-center gap-2 text-red-500"
                   >
                     <i class="fa-solid fa-bug "></i>
-                    <h1 class="text-[12px]">BUG</h1>
+                    <h1 class="text-[12px]">${issue.labels[0] === undefined ? "No Bug" : issue.labels[0]}BUG</h1>
                   </div>
                   <div
                     class="bg-[#FDE68A]/60 rounded-2xl px-2 py-1 flex items-center gap-2 text-[#f18408]"
                   >
-                   <div class="flex items-center gap-1">
+                   <div class="flex items-center ">
                      <i class="fa-solid fa-life-ring"></i>
-                    <h1 class="text-[12px]">HELP WANTED</h1>
+                    <h1 class="text-[12px]">${issue.labels[1] === undefined ? "No Help Need" : issue.labels[1]}</h1>
                    </div>
                   </div>
                 </div>
@@ -73,6 +74,7 @@ const allIssuesDisplay = (issues, id) => {
                 </div>
               </div>
             </div> 
+        
         `;
       cardContainer.append(card);
     }
@@ -106,8 +108,75 @@ closed.addEventListener("click", () => {
 });
 
 const removeBtnColor = () => {
-  const btns = document.querySelectorAll(".btn");
+  const btns = document.querySelectorAll(".btn-issu");
   btns.forEach((btn) => {
     btn.classList.remove("btn-primary");
   });
+};
+
+const modalBtn = (id) => {
+  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`).then(
+    (res) => res.json().then((data) => modalDetailsDisplay(data.data)),
+  );
+};
+
+const modalDetailsDisplay = (word) => {
+  const detailsContainer = document.getElementById("details-container");
+
+  detailsContainer.innerHTML = `
+  <div>
+    <h1 class="font-bold text-xl mb-2">${word.title}</h1>
+
+    <div class="flex gap-7 items-center">
+      <div class="${
+        word.status === "open" ? "bg-green-400" : "bg-purple-400"
+      } px-2 text-[14px] rounded-xl text-white">
+        ${word.status}
+      </div>
+
+      <li class="text-[#647488]">
+        <p class="text-[12px]">Opened by ${word.author}</p>
+      </li>
+
+      <li class="text-[#647488]">
+        <p class="text-[12px]">${word.createdAt}</p>
+      </li>
+    </div>
+
+    <div class="flex flex-wrap items-center gap-2 pb-2 pt-5">
+
+      <div class="bg-red-200 rounded-2xl px-2 py-1 flex items-center gap-2 text-red-500">
+        <i class="fa-solid fa-bug"></i>
+        <h1 class="text-[12px]">${word.labels?.[0] ?? "No"} BUG</h1>
+      </div>
+
+      <div class="bg-[#FDE68A]/60 rounded-2xl px-2 py-1 flex items-center gap-2 text-[#f18408]">
+        <i class="fa-solid fa-life-ring"></i>
+        <h1 class="text-[12px]">${word.labels?.[1] ?? "No Help Need"}</h1>
+      </div>
+
+    </div>
+
+    <p class="text-[#64748B] text-[14px] mt-4">
+      ${word.description}
+    </p>
+
+    <div class="bg-gray-200 p-4 rounded-md flex justify-around mt-5">
+      <div>
+        <p class="text-[#64748B]">Assignee:</p>
+        <p class="font-bold">${word.author}</p>
+      </div>
+
+      <div>
+        <p class="text-[#64748B]">Priority:</p>
+        <div class="bg-red-500 px-3 rounded-xl text-white">
+          ${word.priority}
+        </div>
+      </div>
+    </div>
+
+  </div>
+  `;
+
+  document.getElementById("word_modal").showModal();
 };
--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@ document.getElementById("button-sign").addEventListener("click", () => {
   const pass = document.getElementById("password").value;
   if (username === "admin" && pass === "admin123") {
     // alert("login successful");
-    window.location.href = "./home.html";
+    window.location.href = "home.html";
   } else {
     alert("UserName and Password Not Match");
   }
